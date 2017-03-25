64b2a9558d5f8a750a7c124fe5facbae80e747ec
José Humberto <zehumberto95@gmail.com>
2017-03-25 00:16:53 -0300

    tela de cadastro de usuario

---
+++ View/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $("#data").mask("99/99/9999");
+  $("#cpf").mask("999.999.999-99");
+  $("#telefone").mask("(99) 9999-9999");
+  $("#celular").mask("(99) 9 9999-9999");
+});
